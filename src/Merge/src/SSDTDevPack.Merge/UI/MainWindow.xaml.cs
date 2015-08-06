9c0c3fb6b17cbf4d3b128899cbf443d227ab60b1
Ed Elliott <goeddieuk@gmail.com>
2015-08-06 16:06:54 +0100

    ui finds and displays all types of scripts

--- src/Merge/src/SSDTDevPack.Merge/UI/MainWindow.xaml.cs
+++ src/Merge/src/SSDTDevPack.Merge/UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -15,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EnvDTE;
 using SSDTDevPack.Common.Enumerators;
+using SSDTDevPack.Common.ProjectItems;
 
 namespace SSDTDevPack.Merge.UI
 {
@@ -26,17 +28,23 @@ namespace SSDTDevPack.Merge.UI
         public MainWindow()
         {
             InitializeComponent();
-
+            
             if (Assembly.GetCallingAssembly().FullName != "WinFormHost.Merge, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null")
                 PopulateTreeview();
         }
 
+        private void ClearWindows()
+        {
+            ProjectItems.Items.Clear();
+        }
+
         public void PopulateTreeview()
         {
+            Dispatcher.Invoke(ClearWindows);
+
             var projects = new ProjectEnumerator().Get(ProjectType.SSDT);
 
             var root = Dispatcher.Invoke(() => new TreeViewItem());
-            
             
             foreach (var project in projects)
             {
@@ -54,9 +62,50 @@ namespace SSDTDevPack.Merge.UI
 
         private TreeViewItem GetProjectNode(Project project)
         {
-            var root = new TreeViewItem();
-            root.Header = project.Name;
+            var root = Dispatcher.Invoke(() => new TreeViewItem());
+            Dispatcher.Invoke(() => CreateProjectDefaults(root, project));
+            
+
+
             return root;
+        }
+
+        private void CreateProjectDefaults(TreeViewItem root, Project project)
+        {
+            root.Header = project.Name;
+
+            var preDeploy = GetScripts(project, "PreDeploy");
+            var postDeploy = GetScripts(project, "PostDeploy");
+            var other = GetScripts(project, "None");
+
+            root.Items.Add(preDeploy);
+            root.Items.Add(postDeploy);
+            root.Items.Add(other);
+            
+        }
+
+        private TreeViewItem GetScripts(Project project, string type)
+        {
+            var node = new TreeViewItem();
+            node.Header = type;
+            
+            foreach (ProjectItem item in new ProjectItemEnumerator().Get(project).Where(p => p.HasBuildAction(type)))
+            {
+                node.Items.Add(GetScriptNode(item));
+            }
+            
+            return node;
+
+        }
+
+        private TreeViewItem GetScriptNode(ProjectItem item)
+        {
+            var node = new TreeViewItem();
+            node.Header = item.Name;
+
+            //parse the merge statements...
+
+            return node;
         }
 
         private void ToolbarRefresh_Click(object sender, RoutedEventArgs e)
